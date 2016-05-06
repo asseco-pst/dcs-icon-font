2337f64155b2d3bcdc9f30e25543ea83d8413edc
Orêncio Gonçalves <orencio.goncalves@exictos.com>
2016-05-06 12:14:37 +0100

    Separate the cli from the builder's logic
    So the builder logic can be independent from the cli, it is now separated in differente JS files
    This way we can import the builder for unit testing in a nice and clean way

---
+++ src/builder.js
@@ -0,0 +1,45 @@
+
+import webfontsGenerator from 'webfonts-generator';
+import glob from 'glob';
+
+function build(config) {
+  return new Promise((resolve, reject) => {
+    const files = glob.sync('icons/*.svg');
+
+    try {
+      if (!files.length) {
+        throw new Error('Should provide a non empty file list');
+      }
+
+      if (config.customOpts.help) {
+        resolve(`
+          These are all the available args
+           -- fontName: things
+           -- classPrefix: things
+           -- baseClass: things
+           -- html: things
+          `);
+        return;
+      }
+
+      const options = Object.assign({}, config.webfontsOptions);
+      options.files = files;
+      generateDcsIconFont(options);
+      resolve({ success: true });
+    } catch (e) {
+      reject(e);
+    }
+  });
+}
+
+function generateDcsIconFont(webfontsOptions) {
+  return webfontsGenerator(webfontsOptions, (error, result) => {
+    if (error) throw error;
+
+    return { success: true, result };
+  });
+}
+
+export {
+  build,
+};
