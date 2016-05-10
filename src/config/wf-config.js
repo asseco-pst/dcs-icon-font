f2861c6e242e535ffeacfee8949f3f2bc367194f
Orêncio Gonçalves <orencio.goncalves@exictos.com>
2016-05-10 09:58:42 +0100

    Add cli args for dynamic cssDest and cssFontUrl configuration

--- src/config/wf-config.js
+++ src/config/wf-config.js
@@ -7,18 +7,22 @@
 function getConfig(customOpts = {}) {
   const buildPath = customOpts.out || './build';
   const fontName = customOpts.fontname || 'dcsIconFont';
+  const cssDest = customOpts.cssDest || buildPath;
+
   const webfontsOptions = {
     dest: `${buildPath}/${fontName}`,
-    cssDest: `${buildPath}/${fontName}.css`,
-    cssFontsUrl: fontName,
+    cssDest: `${cssDest}/${fontName}.css`,
+    cssFontsUrl: customOpts.cssFontsUrl || fontName,
     fontName,
     templateOptions: {
       classPrefix: customOpts.classprefix || 'dcs-icon-',
       baseClass: customOpts.baseclass || 'dcs-icon',
     },
     html: customOpts.html || false,
-    htmlDest: `${buildPath}/preview.html`,
+    htmlDest: `${cssDest}/preview.html`,
   };
+
+  console.log(webfontsOptions);
 
   return {
     customOpts,
