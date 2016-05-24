94f17227181a9829bf53cc1058e29fe7a269de7c
Orêncio Gonçalves <orencio.goncalves@exictos.com>
2016-05-24 16:15:51 +0100

    Change unit tests
    Change unit tests according to the new expected behaviour
    
    - Return an error message if the user require HTML preview and Sass at the same time
    - If receiving the '--sass' flag, create a file with _ followed by the font name and with .scss at the end

--- spec/index-spec.js
+++ spec/index-spec.js
@@ -24,7 +24,7 @@ describe('Builder', () => {
       });
     });
 
-    xdescribe('\'--icons\'', () => {
+    describe('\'--icons\'', () => {
       it('should validate if it is not an empty file list', (done) => {
         const options = config.getConfig({ icons: './none' });
 
@@ -48,7 +48,7 @@ describe('Builder', () => {
       });
     });
 
-    xdescribe('\'--baseclass\'', () => {
+    describe('\'--baseclass\'', () => {
       it('should generate the CSS declaring the style for the \'baseclass\' provided', (done) => {
         const baseClass = 'test-icon';
         const options = config.getConfig({ baseclass: baseClass });
@@ -63,7 +63,7 @@ describe('Builder', () => {
       });
     });
 
-    xdescribe('\'--classprefix\'', () => {
+    describe('\'--classprefix\'', () => {
       it('should generate the CSS declaring the style for the \'classPrefix\' provided', (done) => {
         const classPrefix = 'test-icon-';
         const options = config.getConfig({ classprefix: classPrefix });
@@ -78,7 +78,7 @@ describe('Builder', () => {
       });
     });
 
-    xdescribe('\'--fontname\'', () => {
+    describe('\'--fontname\'', () => {
       it('should generate the CSS declaring the style for the \'fontname\' provided', (done) => {
         const fontName = 'testFontName';
         const options = config.getConfig({ fontname: fontName });
@@ -93,7 +93,7 @@ describe('Builder', () => {
       });
     });
 
-    xdescribe('\'--out\'', () => {
+    describe('\'--out\'', () => {
       it('should store all the generated code into the \'out-test\' folder', (done) => {
         const outFolder = 'out-test';
         rimraf.sync(outFolder);
@@ -109,7 +109,7 @@ describe('Builder', () => {
       });
     });
 
-    xdescribe('\'--html\'', () => {
+    describe('\'--html\'', () => {
       it('should generate a html file named \'preview.html\' with a preview for all the icons into the generated font', (done) => {
         const options = config.getConfig({ html: true });
         builder.build(options)
@@ -123,27 +123,38 @@ describe('Builder', () => {
       });
     });
 
-    describe('\'--scss\'', () => {
-      const options = config.getConfig({ scss: true });
-      const outPath = './build';
-      const scssPath = `${outPath}/scss`;
+    describe('\'--html\' and \'--sass\'', () => {
+      it('should return an error message indicating that is there no preview for SASS outputs', (done) => {
+        const options = config.getConfig({ html: true, sass: true });
 
+        builder.build(options)
+          .catch((error) => {
+            expect(error).toEqual(new Error('Is not possible to generate a HTML preview for SASS outputs'));
+            done();
+          });
+      });
+    });
+
+    describe('\'--sass\'', () => {
+      const options = config.getConfig({ sass: true });
+      const outPath = './build';
+      const scssRegex = new RegExp('\\w+.scss');
       it('should generate a SCSS output instead of CSS', (done) => {
         builder.build(options)
           .then((result) => {
-            fs.readdir(`${scssPath}/*.scss`, (err, files) => {
+            fs.readdir(`${outPath}`, (err, files) => {
               expect(files).toBeDefined();
-              expect(files.length).toBeGreaterThan(0);
+              expect(files).toMatch(scssRegex);
               expect(result).toBeSuccessful();
               done();
             });
           });
       });
 
-      it('should generate a _index.scss file', (done) => {
+      it('should generate a _dcsIconFont.scss file', (done) => {
         builder.build(options)
           .then((result) => {
-            fs.readFile(`${scssPath}/_index.scss`, 'utf8', (err, file) => {
+            fs.readFile(`${outPath}/_dcsIconFont.scss`, 'utf8', (err, file) => {
               expect(file).toBeDefined();
               expect(result).toBeSuccessful();
               done();
@@ -154,10 +165,10 @@ describe('Builder', () => {
       it('should generate sizing classes', (done) => {
         builder.build(options)
           .then((result) => {
-            fs.readFile(`${scssPath}/_index.scss`, 'utf8', (err, file) => {
-              expect(file).toContain('-sm');
-              expect(file).toContain('-2x');
-              expect(file).toContain('-5x');
+            fs.readFile(`${outPath}/_dcsIconFont.scss`, 'utf8', (err, file) => {
+              expect(file).toContain('sm');
+              expect(file).toContain('2x');
+              expect(file).toContain('5x');
               expect(result).toBeSuccessful();
               done();
             });
