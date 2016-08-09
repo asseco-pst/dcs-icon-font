895281b814bf4379e41fe94a07cd6a4e6fcf9b81
orenciorodolfo <orenciorodolfo@hotmail.com>
2016-08-09 13:35:31 +0100

    Refactor 'build' promises
    Remove nested promises and use 'reject' instead of throw in order to handle it into the 'catch' function

--- src/builder.js
+++ src/builder.js
@@ -8,41 +8,44 @@ import glob from 'glob';
  * @return {Object} a successful response or an error in case it occurs
  */
 function build(config = {}) {
+  // if it is a --help access
+  if (config.customOpts && (config.customOpts.help)) {
+    return Promise.resolve(`
+These are all the available arguments:
+--out [String]: specifies where the generated code is stored into. Default "./build"
+--icons [String]: specifies the directory that contains the SVG icons for which you want to generate the font. Default "./icons"
+--baseclass [String]: specifies the base css class name. Default "dcs-icon"
+--classprefix [String]: specifies the css class prefix for all your icons. Default "dcs-icon-"
+--fontname [String]: the name for your brand new font. Default "dcsIconFont"
+--html: enables the generation of a html file with a preview for all the icons in the generated font
+    `);
+  }
+
   return new Promise((resolve, reject) => {
     try {
-      if (config.customOpts && config.customOpts.help) {
-        resolve(`
-These are all the available arguments:
- --out [String]: specifies where the generated code is stored into. Default "./build"
- --icons [String]: specifies the directory that contains the SVG icons for which you want to generate the font. Default "./icons"
- --baseclass [String]: specifies the base css class name. Default "dcs-icon"
- --classprefix [String]: specifies the css class prefix for all your icons. Default "dcs-icon-"
- --fontname [String]: the name for your brand new font. Default "dcsIconFont"
- --html: enables the generation of a html file with a preview for all the icons in the generated font
-          `);
-        return;
-      }
-
       const iconsPath = config.customOpts.icons || 'icons/*.svg';
       const files = glob.sync(iconsPath);
 
       if (!files.length) {
-        throw new Error('Invalid file list');
+        reject(new Error('Invalid file list'));
       }
 
       if (config.customOpts.sass && config.customOpts.html) {
-        throw new Error('Is not possible to generate a HTML preview for SASS outputs');
+        reject(new Error('Is not possible to generate a HTML preview for SASS outputs'));
       }
 
       const options = Object.assign({}, config.webfontsOptions);
       options.files = files;
 
-      generateDcsIconFont(options)
-        .then(() => resolve({ success: true }))
-        .catch((e) => reject(e));
+      resolve(options);
     } catch (e) {
       reject(e);
     }
+  })
+  .then((options) => generateDcsIconFont(options))
+  .then((resolve) => resolve({ success: true }))
+  .catch((e) => {
+    throw e;
   });
 }
 
